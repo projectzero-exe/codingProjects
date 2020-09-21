00b220fa17f4295141a0a402345775f66ee33dc0
projectzero-exe <granbestia001@gmail.com>
2020-09-21 14:07:21 -0500

    experimenting with requests library/study module: added format block of code to observe the structure

--- get_network.py
+++ get_network.py
@@ -2,7 +2,7 @@ import requests
 from dnac_config import IP2
 from get_auth_token import get_auth_token
 from pprint import pprint
-import json
+
 
 def get_device_list():
 
@@ -11,26 +11,18 @@ def get_device_list():
 
     hdr = {'x-auth-token': token, 'content-type': 'application/json'}
     r = requests.get(IP2, headers=hdr, verify=False)
-    todos = r.json()
-    print_device_list(todos)
+    #todos = r.json()
+    print(r.json())
+    print_device_list(r.json())
 
 def print_device_list(device_json):
-    print("{0:42}{1:17}{2:12}{3:18}{4:12}{5:16}{6:15}".
+    print("{0:42}{1:17}{2:16}{3:18}{4:12}{5:16}{6:15}".
               format("hostname", "mgmt IP", "serial", "platformId", "SW Version", "role", "Uptime"))
     for device in device_json['response']:
-            uptime = "N/A" if device['upTime'] is None else device['upTime']
-            if device['serialNumber'] is not None and "," in device['serialNumber']:
-                serialPlatformList = zip(device['serialNumber'].split(","), device['platformId'].split(","))
-            else:
-                serialPlatformList = [(device['serialNumber'], device['platformId'])]
-            for (serialNumber, platformId) in serialPlatformList:
-                print("{0:42}{1:17}{2:12}{3:18}{4:12}{5:16}{6:15}".
-                      format(device['hostname'],
-                             device['managementIpAddress'],
-                             serialNumber,
-                             platformId,
-                             device['softwareVersion'],
-                             device['role'], uptime))
+        print("{0:42}{1:17}{2:16}{3:18}{4:12}{5:16}{6:15}".format(device['hostname'], device['managementIpAddress'],
+                                                                  device['serialNumber'], device['platformId'],
+                                                                  device['softwareVersion'], device['role'],
+                                                                  device['upTime']))
 
     # print(type(todos))
     # print(todos)
