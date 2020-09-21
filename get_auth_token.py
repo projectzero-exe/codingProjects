0a93fe75f5607a8335ec6cd2ec2bcc84a5391f69
projectzero-exe <granbestia001@gmail.com>
2020-09-21 12:37:30 -0500

    working script to post "Authentication" take the token and request info from endpoint

--- get_auth_token.py
+++ get_auth_token.py
@@ -1,7 +1,9 @@
 import requests
 from requests.auth import HTTPBasicAuth
 
-from dnac_config import USERNAME, PASSWORD, DNAC_IP, DNAC_PORT
+from dnac_config import UN, PW, IP
+
+requests.urllib3.disable_warnings()
 
 
 def get_auth_token():
@@ -10,20 +12,13 @@ def get_auth_token():
     to make a call to the Auth Endpoint
     """
 
-    url = 'https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token'
-    r = requests.post(url, auth=HTTPBasicAuth(USERNAME, PASSWORD),
+    url = IP
+    r = requests.post(url, auth=HTTPBasicAuth(UN, PW),
                       headers={'content-type': 'application/json'}, verify=False)
-    token= r.json()['Token']
-    print("Token retrieved from DNA Center is: {}".format(token))
+    token = r.json()['Token']
+    #print(f"Token retrieved from DNA Center is: {token}")
     return token
+
 
 if __name__ == "__main__":
     get_auth_token()
-
-<<<<<<< HEAD
-
-print('RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRAAAAAAAAAAAAAAAAAAWWWWWWWWWWWWWWWWWWWWWWRRRRRRRRRRRRRRRRRffffggg')
-
-print('lorem ipsum')
-=======
->>>>>>> parent of c0612d7... made adjustments
--- get_network.py
+++ get_network.py
@@ -1,30 +1,26 @@
 import requests
-from requests.auth import HTTPBasicAuth
-import get_auth_token
-from dnac_config import USERNAME, PASSWORD
-import pprint
+from dnac_config import IP2
+from get_auth_token import get_auth_token
+from pprint import pprint
+import json
 
 def get_device_list():
+
     token = get_auth_token()
-    url = 'https://sandboxdnac.cisco.com/api/v1/network-device'
+
+
     hdr = {'x-auth-token': token, 'content-type': 'application/json'}
-    r = requests.get(url, headers=hdr, verify=False)
-    device_list = r.json()
-    #print(device_list)
-    pprint.pprint(device_list)
+    r = requests.get(IP2, headers=hdr, verify=False)
+    todos = r.json()
+    print(type(todos))
+    print(todos)
+    todos['response'] = [dict(hostname=k1['hostname'],managementIpAddress=k1['managementIpAddress'])
+                         for k1 in todos['response']] #this way i can call on specific key values return from the response
 
-def get_auth_token():
-    """
-    Building out Auth request. Using requests.post
-    to make a call to the Auth Endpoint
-    """
+    pprint(type(todos))
+    pprint(todos)
 
-    url = 'https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token'
-    r = requests.post(url, auth=HTTPBasicAuth(USERNAME, PASSWORD),
-                      headers={'content-type': 'application/json'}, verify=False)
-    token = r.json()['Token']
-    print("Token retrieved from DNA Center is: {}".format(token))
-    return token
+
 
 if __name__ == '__main__':
     get_device_list()
