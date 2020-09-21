01c60f9fa74c6d640dd3603f52bb9bd9c49d5056
projectzero-exe <granbestia001@gmail.com>
2020-09-21 12:38:50 -0500

    DNA center info for Cisco's always on sandbox

---
+++ dnac_config.py
@@ -0,0 +1,9 @@
+
+
+IP = 'https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token'
+IP2 = 'https://sandboxdnac.cisco.com/api/v1/network-device'
+PORT = 443
+
+UN = "devnetuser"
+PW = "Cisco123!"
+VERSION = "v1"
