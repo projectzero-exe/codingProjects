0c614b41720bcec10bb50ac95ce4ea01c7bfbaca
projectzero-exe <granbestia001@gmail.com>
2020-10-02 21:13:36 -0500

    object oriented programming; added borders condition

--- robot_test.py
+++ robot_test.py
@@ -4,3 +4,6 @@ r2 = Robot("Tom", 8000)
 
 print("His name is", r2.name, r2.year)
 
+r3 = Robot('Dool', 2121)
+
+print("His name is" + r3.name, 'and was born in' + r3.year)
--- script_portfolio/Cntry_App.py
+++ script_portfolio/Cntry_App.py
@@ -13,37 +13,43 @@ formatted = json.dumps(json_result, indent=4)
 print(formatted)
 
 """
-# print('What info would you like query about a country? Select and choose a number.\n 1. Population\n '
-#       '2. Languages\n 3. Timezone')
-# option = int(input(">>> "))
-# print(f"You chose {option}.")
+print('What info would you like query about a country? Select and choose a number.\n 1. Population\n '
+      '2. Languages\n 3. Timezone\n 4. Borders')
+option = int(input(">>> "))
+print(f"You chose {option}.")
 
 print('What country do you want that information for?')
 country = input('>>> ')
-#params = {'fields': 'population;languages;timezones', 'fullText': 'true'}
+params = {'fields': 'population;languages;timezones;borders', 'fullText': 'true'}
 
 r = requests.get(base_url + f'name/{country}?')
 
 json_result = r.json()
-pprint(json_result)
+#pprint(json_result)
 #formatted = json.dumps(json_result, indent=4)
 #
 #pprint.pprint(json_result)
 
-country = json_result[0]['borders']
+country = json_result[0]
 
-print(country)
-#
-# if option == 1:
-#     populus = country['population']
-#     print('Population is: {}.'.format(populus))
-#
-# elif option == 2:
-#     langs = []
-#     for language in country['languages']:
-#         langs.append(language['name'])
-#     print('The languages are: {}'.format(', '.join(langs)))
-#
-# else:
-#     timez = country['timezones']
-#    print('The Timezones are: {}'.format(', '.join(timez)))
+#print(country)
+
+if option == 1:
+    populus = country['population']
+    print('Population is: {}.'.format(populus))
+
+elif option == 2:
+    langs = []
+    for language in country['languages']:
+        langs.append(language['name'])
+    print('The languages are: {}'.format(', '.join(langs)))
+
+elif option == 4:
+    grenze = []
+    for borders in country['borders']:
+        grenze.append(borders)
+    print('Countries surrounding its borders: {}'.format(', '.join(grenze)))
+
+else:
+    timez = country['timezones']
+    print('The Timezones are: {}'.format(', '.join(timez)))
