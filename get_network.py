87367c216cf04c3716ae3821b05d6e175aa3d4f1
projectzero-exe <granbestia001@gmail.com>
2020-10-03 21:08:01 -0500

    simplified the output layout w/ f-str l19,21,20 & 26

--- get_network.py
+++ get_network.py
@@ -11,24 +11,20 @@ def get_device_list():
     #params = {'response': 'family;routers', 'fullText': 'true'}
     hdr = {'x-auth-token': token, 'content-type': 'application/json'}
     r = requests.get(IP2, headers=hdr, verify=False)
-    pprint(r.json()['response'])
+    #pprint(r.json()['response'])
     print_device_list(r.json())
 
 
 def print_device_list(device_json):
-    print(f"{'HostName':42}{'MgmtIpAddress':17}{'SerialNumber':16}{'PlatformId':18}{'SoftwareVersion':18}{'Role':16}{'UpTime':15}")
+    print(f"{'HostName':42}{'MgmtIpAddress':17}{'SerialNumber':16}{'PlatformId':18}"
+          f"{'SoftwareVersion':18}{'Role':16}{'UpTime':15}")
     print(f'{"-" * 147}')
     for device in device_json['response']:
-        print(f"{device['hostname']:42}{device['managementIpAddress']:17}{device['serialNumber']:16}{device['platformId']:18}{device['softwareVersion']:18}{device['role']:16}{device['upTime']:15}")
+        print(f"{device['hostname']:42}{device['managementIpAddress']:17}{device['serialNumber']:16}"
+              f"{device['platformId']:18}{device['softwareVersion']:18}{device['role']:16}{device['upTime']:15}")
 
     print(f'{"-" * 147}')
-    # print(type(todos))
-    # print(todos)
-    # todos['response'] = [dict(hostname=k1['hostname'],managementIpAddress=k1['managementIpAddress'], )
-    #                      for k1 in todos['response']] #this way i can call on specific key values return from the response
-    # # data = json.dumps(todos)
-    # pprint(type(todos))
-    # pprint(todos)
+
 
 if __name__ == '__main__':
     get_device_list()
