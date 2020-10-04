c352036b497141375342ce89d5e266477e259d89
projectzero-exe <granbestia001@gmail.com>
2020-10-03 20:55:47 -0500

    simplified the out layout starting line 19 and working on 21

--- get_network.py
+++ get_network.py
@@ -1,29 +1,29 @@
 import requests
 from dnac_config import IP2
 from get_auth_token import get_auth_token
-
+from pprint import pprint
 
 
 def get_device_list():
 
     token = get_auth_token()
 
-
+    #params = {'response': 'family;routers', 'fullText': 'true'}
     hdr = {'x-auth-token': token, 'content-type': 'application/json'}
     r = requests.get(IP2, headers=hdr, verify=False)
-    #todos = r.json()
-    print(r.json())
+    pprint(r.json()['response'][0])
     print_device_list(r.json())
 
+
 def print_device_list(device_json):
-    print("{0:42}{1:17}{2:16}{3:18}{4:12}{5:16}{6:15}".
-              format("hostname", "mgmt IP", "serial", "platformId", "SW Version", "role", "Uptime"))
+    print(f"{'HostName':42}{'MgmtIpAddress':17}{'SerialNumber':16}{'PlatformId':18}{'SoftwareVersion':18}{'Role':16}{'UpTime':15}")
+    print(f'{"-" * 147}')
     for device in device_json['response']:
-        print("{0:42}{1:17}{2:16}{3:18}{4:12}{5:16}{6:15}".format(device['hostname'], device['managementIpAddress'],
+        print("{0:42}{1:17}{2:16}{3:18}{4:18}{5:16}{6:15}".format(device['hostname'], device['managementIpAddress'],
                                                                   device['serialNumber'], device['platformId'],
                                                                   device['softwareVersion'], device['role'],
                                                                   device['upTime']))
-
+    print(f'{"-" * 147}')
     # print(type(todos))
     # print(todos)
     # todos['response'] = [dict(hostname=k1['hostname'],managementIpAddress=k1['managementIpAddress'], )
