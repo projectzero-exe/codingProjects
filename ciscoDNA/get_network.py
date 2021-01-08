d776209bc5cc0aeb0fd712762ff62d5a2cae5716
projectzero-exe <70857618+projectzero-exe@users.noreply.github.com>
2021-01-08 08:54:46 -0600

    Retrieves Network devices registered to DNA cloud

---
+++ ciscoDNA/get_network.py
@@ -0,0 +1,30 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import get_auth_token
+from dnac_config import USERNAME, PASSWORD
+import pprint
+
+def get_device_list():
+    token = get_auth_token()
+    url = 'https://sandboxdnac.cisco.com/api/v1/network-device'
+    hdr = {'x-auth-token': token, 'content-type': 'application/json'}
+    r = requests.get(url, headers=hdr, verify=False)
+    device_list = r.json()
+    #print(device_list)
+    pprint.pprint(device_list)
+
+def get_auth_token():
+    """
+    Building out Auth request. Using requests.post
+    to make a call to the Auth Endpoint
+    """
+
+    url = 'https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token'
+    r = requests.post(url, auth=HTTPBasicAuth(USERNAME, PASSWORD),
+                      headers={'content-type': 'application/json'}, verify=False)
+    token = r.json()['Token']
+    print(f"Token retrieved from DNA Center is: {token}")
+    return token
+
+if __name__ == '__main__':
+    get_device_list()
